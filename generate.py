ebbca9d7bdb92f6e43eb580ccd00d3d70ad91bc6
ahmedWakil <raiyan33380@gmail.com>
2023-10-11 18:55:06 -0400

    added in main funcitonal files with some of the data files

---
+++ generate.py
@@ -0,0 +1,20 @@
+def Determinants():
+    pass
+
+def Sentence():
+    return NounPhrase() + VerbPhrase()
+
+def VerbPhrase():
+    pass
+
+def NounPhrase():
+    pass
+
+def AdjecivePhrase():
+    pass
+
+def PrepositionalPhrase():
+    pass
+
+def AdverbialPhrase():
+    pass
